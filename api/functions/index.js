b270b1aa9449b7f943d3c6efa9cae9fbef6b8b46
cyberVandal <goran_mitkovski@yahoo.com>
2020-06-18 01:07:33 +0200

    User Auth

--- api/functions/index.js
+++ api/functions/index.js
@@ -5,13 +5,19 @@ const {
     getAllTodos,
     postOneTodo,
     deleteTodo,
-    editTodo
+    editTodo,
+
 } = require('./APIs/todos')
+
+const {
+    loginUser
+} = require('./APIs/users')
+
 
 app.get('/todos', getAllTodos);
 app.post('/todo', postOneTodo);
 app.delete('/todo/:todoId', deleteTodo);
 app.put('/todo/:todoId', editTodo);
-
+app.post('/login', loginUser);
 
 exports.api = functions.https.onRequest(app);
